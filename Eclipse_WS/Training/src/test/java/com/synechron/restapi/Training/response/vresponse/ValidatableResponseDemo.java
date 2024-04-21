2839b5f588b9f27d9555091fe4077f88309bc7ed
Ab007007 <aru03.info@gmail.com>
2024-04-21 12:31:20 +0530

    validatable response

--- Eclipse_WS/Training/src/test/java/com/synechron/restapi/Training/response/vresponse/ValidatableResponseDemo.java
+++ Eclipse_WS/Training/src/test/java/com/synechron/restapi/Training/response/vresponse/ValidatableResponseDemo.java
@@ -1,4 +1,4 @@
-package com.synechron.restapi.Training.response.jsonpath;
+package com.synechron.restapi.Training.response.vresponse;
 
 import java.util.Iterator;
 import java.util.List;
@@ -17,12 +17,12 @@ import com.synechron.restapi.Training.global.GlobalVariables;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
-public class ValidateResponseBodyUsingJsonPath {
+public class ValidatableResponseDemo {
 
 	
-	Response response = null;
-	JsonPath responseBody =null;
+	ValidatableResponse response = null;
 	
 	@BeforeTest
 	public void getResponse()
@@ -34,10 +34,12 @@ public class ValidateResponseBodyUsingJsonPath {
 						param("key", GlobalVariables.key).
 						param("token", GlobalVariables.token).
 					when().
-						get("https://api.trello.com/1/boards/"+GlobalVariables.boardID);
+						get("https://api.trello.com/1/boards/"+GlobalVariables.boardID).
+					then().assertThat().statusCode(200);
+		
 		//response.prettyPrint();
 		
-		responseBody = new JsonPath(response.asString());
+		
 		System.out.println("Response is saved in a response object");
 		
 	
@@ -63,7 +65,7 @@ public class ValidateResponseBodyUsingJsonPath {
 		System.out.println("Test to Print ID");
 //		String id = JsonPath.read(response.asString(), "$.id");
 		
-		String id = responseBody.get("id");
+		String id = response.extract().path("id");
 		System.out.println("Printing ID " + id);
 		Assert.assertEquals(GlobalVariables.boardID, id);
 	}
@@ -72,7 +74,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	public void getName()
 	{
 		System.out.println("Test to Print Name");
-		String name = new JsonPath(response.asString()).get("name");
+		String name = response.extract().path("name");
 		System.out.println("Printing Name " + name);
 	}
 	
@@ -80,10 +82,10 @@ public class ValidateResponseBodyUsingJsonPath {
 	public void getAllUrlsFrombackgroundImageScaled()
 	{
 		System.out.println("Test to Print All URLs");
-		int totalUrls  =   responseBody.get("prefs.backgroundImageScaled.size()");
+		int totalUrls  =   response.extract().path("prefs.backgroundImageScaled.size()");
 		for (int i = 0; i < totalUrls ; i++) 
 		{
-			String url = responseBody.get("prefs.backgroundImageScaled[" + i +"].url");
+			String url = response.extract().path("prefs.backgroundImageScaled[" + i +"].url");
 			System.out.println(url);
 		}
 		
@@ -92,7 +94,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	@Test(priority = 4)
 	public void getFirstElementInBGIScaled()
 	{
-		Map<String, ?> item = responseBody.get("prefs.backgroundImageScaled[0]");
+		Map<String, ?> item = response.extract().path("prefs.backgroundImageScaled[0]");
 		Set<String> keys = item.keySet();
 		for (String key : keys) {
 			System.out.println("key is " + key + "  and the value is " + item.get(key));
@@ -103,7 +105,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	public void getAllBGIScaled()
 	{
 		
-		List<Map<String, ?>> allElements = responseBody.get("prefs.backgroundImageScaled");
+		List<Map<String, ?>> allElements = response.extract().path("prefs.backgroundImageScaled");
 		System.out.println("Total number of maps present in list " + allElements.size());
 		
 		for (int i = 0; i < allElements.size() ; i++) {
@@ -119,7 +121,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	@Test(priority = 6)
 	public void getAllWidthGreaterThan1000()
 	{
-		List<Map<String, ?>> allElements = responseBody.get("prefs.backgroundImageScaled.findAll { it.width > 1000 }");
+		List<Map<String, ?>> allElements = response.extract().path("prefs.backgroundImageScaled.findAll { it.width > 1000 }");
 		System.out.println("Total number of maps present in list " + allElements.size());
 		
 		for (int i = 0; i < allElements.size() ; i++) {
@@ -135,7 +137,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	@Test(priority = 7)
 	public void getAllUrlWithWidthGreaterThan1000()
 	{
-		List<String> allUrls = responseBody.get( "prefs.backgroundImageScaled.findAll { it.width > 1000 }.url");
+		List<String> allUrls = response.extract().path( "prefs.backgroundImageScaled.findAll { it.width > 1000 }.url");
 		for (String url : allUrls) {
 			System.out.println(url);
 		}
@@ -145,7 +147,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	@Test(priority = 8)
 	public void getElementWithMaxWidthInBGIScaled()
 	{
-		Map<String, ?> item = responseBody.get("prefs.backgroundImageScaled.max { it.width }");
+		Map<String, ?> item = response.extract().path("prefs.backgroundImageScaled.max { it.width }");
 		Set<String> keys = item.keySet();
 		for (String key : keys) {
 			System.out.println("key is " + key + "  and the value is " + item.get(key));
@@ -156,7 +158,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	@Test(priority = 9)
 	public void getElementWithMinWidthInBGIScaled()
 	{
-		Map<String, ?> item = responseBody.get("prefs.backgroundImageScaled.min { it.width }");
+		Map<String, ?> item = response.extract().path("prefs.backgroundImageScaled.min { it.width }");
 		Set<String> keys = item.keySet();
 		for (String key : keys) {
 			System.out.println("key is " + key + "  and the value is " + item.get(key));
@@ -167,7 +169,7 @@ public class ValidateResponseBodyUsingJsonPath {
 	@Test(priority = 9)
 	public void getURLWithMinWidthInBGIScaled()
 	{
-		String url = responseBody.get("prefs.backgroundImageScaled.min { it.width }.url");
+		String url = response.extract().path("prefs.backgroundImageScaled.min { it.width }.url");
 		System.out.println(url);
 	}
 	
